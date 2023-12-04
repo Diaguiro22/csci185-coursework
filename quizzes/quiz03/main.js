b1a81a8ffc054844b6152f3f89e23f49c7d57d59
Diana <daguilar@unca.edu>
2023-12-04 16:38:58 -0500

    Quiz 3

---
+++ quizzes/quiz03/main.js
@@ -0,0 +1,47 @@
+function color1() {
+    const square1 = document.querySelector("square1"); 
+
+    function makeBlue(){
+        document.querySelector("square1").style.backgroundColor = "blue"; 
+    }
+}
+
+function color2() {
+    const square2 = document.querySelector("square2"); 
+
+    function makeGreen(){
+        document.querySelector("square2").style.backgroundColor = "green"; 
+    }
+}
+
+function color3() {
+    const square3 = document.querySelector("square3"); 
+
+    function makeBrown(){
+        document.querySelector("square3").style.backgroundColor = "brown"; 
+    }
+}
+
+function color4() {
+    const square4 = document.querySelector("square4"); 
+
+    function makePurple(){
+        document.querySelector("square4").style.backgroundColor = "purple"; 
+    }
+}
+
+function color5() {
+    const square5 = document.querySelector("square5"); 
+
+    function makeGreen(){
+        document.querySelector("square5").style.backgroundColor = "yellow"; 
+    }
+}
+
+function color6() {
+    const square6 = document.querySelector("square6"); 
+
+    function makeGreen(){
+        document.querySelector("square6").style.backgroundColor = "pink"; 
+    }
+}
